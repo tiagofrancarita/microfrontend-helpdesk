3474c5c64f743dfa69031956dad97ec7143d9eb9
Tiago França <tiagofranca.rita@gmail.com>
2024-06-26 12:26:23 -0300

    created and implements component tecnico-list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild,OnInit} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [ 
+    {id: 1, nome: 'João', cpf: '123.456.789-00', email: 'joao.silva@bol.com', senha: '123', perfis: ['0'], dataCriacao: '26/06/2024'},];
+
+  displayedColumns: string[] = ['cod', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
