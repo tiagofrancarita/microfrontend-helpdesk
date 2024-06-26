2aba43349b7d9f99971f448e49301fbd80fb2dbe
Tiago França <tiagofranca.rita@gmail.com>
2024-06-26 00:00:43 -0300

    criacao do component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,11 +28,13 @@ import { MatCardModule } from '@angular/material/card';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxMaskModule } from 'ngx-mask';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
